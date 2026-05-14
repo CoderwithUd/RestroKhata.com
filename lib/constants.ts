e4b315ba6e9777a8627a65ad8a9fd4e234f99007
CoderwithUd <udayman178vi@gmail.com>
2026-05-14 13:14:16 +0530

    api url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 ﻿export const RAW_API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "https://restro-backend-hpx8.onrender.com";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.restrokhata.com";
     // process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
 
 export const API_BASE_URL = RAW_API_BASE_URL.endsWith("/api")
